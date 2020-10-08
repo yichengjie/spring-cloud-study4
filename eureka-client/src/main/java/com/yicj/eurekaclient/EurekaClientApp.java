f44154c7ba1c4c0a639174b706ddb89b8dd527db
易成杰 <626659321@qq.com>
2020-10-08 16:28:08 +0800

    保存

---
+++ eureka-client/src/main/java/com/yicj/eurekaclient/EurekaClientApp.java
@@ -0,0 +1,12 @@
+package com.yicj.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApp.class, args) ;
+    }
+}
