52fa06a002badabf79868db7ba8ef4463f4b6169
易成杰 <626659321@qq.com>
2020-10-10 10:15:07 +0800

    zuul基本用法

--- client-a/src/main/java/com/yicj/eurekaclient/controller/TestController.java
+++ client-a/src/main/java/com/yicj/eurekaclient/controller/TestController.java
@@ -1,12 +1,16 @@
 package com.yicj.eurekaclient.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class TestController {
     @GetMapping("/add")
-    public Integer add(Integer a, Integer b){
+    public Integer add(Integer a, Integer b, @RequestHeader(value = "X-Token", required = false) String token){
+        log.info("=======> token : " + token);
         return a + b ;
     }
 }
