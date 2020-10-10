8df1ead1ad8130015c90f1afd0c327ab2c701174
易成杰 <626659321@qq.com>
2020-10-10 09:15:44 +0800

    保存

--- client-a/src/main/java/com/yicj/eurekaclient/controller/TestController.java
+++ client-a/src/main/java/com/yicj/eurekaclient/controller/TestController.java
@@ -3,7 +3,7 @@ package com.yicj.eurekaclient.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/client")
 public class TestController {
     @GetMapping("/add")
     public Integer add(Integer a, Integer b){
